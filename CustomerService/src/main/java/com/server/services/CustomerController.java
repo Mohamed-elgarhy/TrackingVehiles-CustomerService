f272a97673663c8adf399b2a23ca880754a9d053
mohamed ezzeldin <m_elgarhy@yahoo.com>
2018-09-16 17:08:49 +0200

    code refactor and test cases

--- CustomerService/src/main/java/com/server/services/CustomerController.java
+++ CustomerService/src/main/java/com/server/services/CustomerController.java
@@ -1,16 +1,16 @@
 package com.server.services;
 
-import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
-import javax.ws.rs.PathParam;
 import javax.ws.rs.core.Response;
 import javax.ws.rs.core.Response.Status;
 
-import org.apache.http.HttpStatus;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -21,24 +21,54 @@ public class CustomerController {
 	private CustomerRepository customerRepository;
 
 	@GetMapping("/customers")
-	public List<Customer> retrieveVechileBeanList() {
-
-		List<Customer> customers = new ArrayList<>();
-		customers = customerRepository.findAll();
-
-		return customers;
-	}
-	
-	@GetMapping("/customer")
-	public Response retrieveVechileBean(@RequestParam("id") Long customerId) {
+	public List<Customer> retrieveCustomerList() {
 
 		
+		return customerRepository.findAll();
+	}
+
+	@GetMapping("/customer")
+	public Response retrieveCustomer(@RequestParam("id") Long customerId) {
+
 		Optional<Customer> customer = customerRepository.findById(customerId);
 
 		if (customer.isPresent())
 			return Response.ok().entity(customer.get()).build();
 		else
 			return Response.status(Status.NOT_FOUND).build();
-		
+
+	}
+
+	@RequestMapping(value = "/customer", method = RequestMethod.PUT)
+	public Response createCustomer(@RequestBody Customer customer) {
+
+		customerRepository.save(customer);
+
+		return Response.ok("success").build();
+	}
+
+	@RequestMapping(value = "/customer", method = RequestMethod.POST)
+	public Response updateCustomer(@RequestBody Customer customer) {
+
+		Optional<Customer> objectToUpdate = customerRepository.findById(customer.getId());
+
+		if (objectToUpdate.isPresent()) {
+			Customer updatedObject = objectToUpdate.get();
+
+			if (customer.getCustomerAddress() != null
+					&& !updatedObject.getCustomerAddress().equalsIgnoreCase(customer.getCustomerAddress()))
+				updatedObject.setCustomerAddress(customer.getCustomerAddress());
+
+			if (customer.getCustomerName() != null
+					&& !updatedObject.getCustomerName().equalsIgnoreCase(customer.getCustomerName()))
+				updatedObject.setCustomerName(customer.getCustomerName());
+
+			customerRepository.save(updatedObject);
+		} else {
+			return Response.status(Status.NOT_FOUND).entity("Object not found").build();
+
+		}
+
+		return Response.ok("success").build();
 	}
 }
--- CustomerService/src/test/java/com/server/services/CustomerServiceApplicationTests.java
+++ CustomerService/src/test/java/com/server/services/CustomerServiceApplicationTests.java
@@ -1,118 +1,141 @@
 package com.server.services;
 
 import static org.hamcrest.CoreMatchers.containsString;
-import static org.hamcrest.Matchers.hasSize;
 import static org.hamcrest.core.Is.is;
-import static org.junit.Assert.assertThat;
-import static org.mockito.BDDMockito.given;
-import static org.mockito.Matchers.contains;
-import static org.mockito.Matchers.isNull;
-import static org.mockito.Matchers.matches;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-import java.util.Collections;
-import java.util.List;
-
-import javax.ws.rs.core.Response;
+import javax.ws.rs.core.MediaType;
 
 import org.hamcrest.core.IsNull;
+import org.json.JSONObject;
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
-import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 
 @RunWith(SpringRunner.class)
 @AutoConfigureMockMvc()
 @SpringBootTest()
-//@ContextHierarchy(@ContextConfiguration)
+// @ContextHierarchy(@ContextConfiguration)
 public class CustomerServiceApplicationTests {
 
-	   @Autowired
-	   private MockMvc mvc;
+	@Autowired
+	private MockMvc mvc;
 
-	  /*@MockBean
-	   private CustomerController customerController;*/
+	@InjectMocks
+	private CustomerController customerController;
 
-	   @Test
-	    public void shouldReturnAllCustomersAsJson() throws Exception {
-	        this.mvc.perform(get("/customers")).andDo(print()).andExpect(status().isOk())
-	        //;
-	        .andExpect(jsonPath("$[0].id", is(1)))
-	        .andExpect(jsonPath("$[0].customerName", is("Kalles Grustransporter")))
-	        .andExpect(jsonPath("$[0].customerAddress", containsString("Cement"))); //string(containsString((("Hello World")))));
-	    }
-	   
-	   @Test
-	    public void shouldReturnOneCustomersAsJson() throws Exception {
-	        this.mvc.perform(get("/customer").param("id", "1")).andDo(print()).andExpect(status().isOk())
-	        //;
-	        .andExpect(jsonPath("$.entity.id", is(1)))
-	        .andExpect(jsonPath("$.entity.customerName", is("Kalles Grustransporter")))
-	        .andExpect(jsonPath("$.entity.customerAddress", containsString("Cement"))); //string(containsString((("Hello World")))));
-	    }
-	   
-	   @Test
-	    public void shouldReturnNotFoundStatusAndNullObject() throws Exception {
-	        this.mvc.perform(get("/customer").param("id", "7")).andDo(print()).andExpect(status().isOk())
-	        .andExpect(jsonPath("$.status", is(404)))
-	        .andExpect(jsonPath("$.entity").value(IsNull.nullValue())); //string(containsString((("Hello World")))));
-	    }
-	   
-	  /* @Test
-	    public void shouldReturnOneCustomersAsJson1() throws Exception {
-		   assertThat(this.customerController).isNotNull();
-		    when(customerController.retrieveVechileBean(("1")).thenReturn(Response.ok().entity(new Customer(1L, "abc", "street")).build());
-		 
-		    MvcResult result= mockMvc.perform(get("/product/{id}/", 1))
-		            .andExpect(status().isOk())
-		            .andExpect(view().name("productshow"))
-		            .andExpect(MockMvcResultMatchers.model().attributeExists("product"))
-		            .andExpect(model().attribute("product", hasProperty("id", is(1))))
-		            .andExpect(model().attribute("product", hasProperty("productId", is("235268845711068308"))))
-		            .andExpect(model().attribute("product", hasProperty("description", is("Spring Framework Guru Shirt"))))
-		            .andExpect(model().attribute("product", hasProperty("price", is(new BigDecimal("18.95")))))
-		            .andExpect(model().attribute("product", hasProperty("imageUrl", is("https://springframework.guru/wp-content/uploads/2015/04/spring_framework_guru_shirt-rf412049699c14ba5b68bb1c09182bfa2_8nax2_512.jpg"))))
-		            .andReturn();
-	    }*/
-	   
-	   
-	   /*@Test
-	   public void getArrivals() throws Exception {
-	       Customer customer = new Customer();
-	       customer.setCustomerName("Yerevan");
+	@Mock
+	CustomerRepository customerRepository;
 
-	       List<Customer> allCustomers = Collections.singletonList(customer);
+	@Before
+	public void setUp() {
+		MockitoAnnotations.initMocks(this);
+	}
 
-	       given(customerController.retrieveVechileBeanList()).willReturn(allCustomers);
+	@Test
+	public void shouldReturnAllCustomersAsJson() throws Exception {
+		this.mvc.perform(get("/customers")).andDo(print()).andExpect(status().isOk())
+				// ;
+				.andExpect(jsonPath("$[0].id", is(1)))
+				.andExpect(jsonPath("$[0].customerName", is("Kalles Grustransporter")))
+				.andExpect(jsonPath("$[0].customerAddress", containsString("Cement"))); // string(containsString((("Hello
+																						// World")))));
+	}
 
-	       mvc.perform(get("/customers"))
-	               .andExpect(status().isOk())
-	               .andExpect(jsonPath("$", hasSize(1)))
-	               .andExpect(jsonPath("$[0].city", is(customer.getCustomerName())));
-	   }*/
+	@Test
+	public void shouldReturnOneCustomersAsJson() throws Exception {
+		this.mvc.perform(get("/customer").param("id", "1")).andDo(print()).andExpect(status().isOk())
+				// ;
+				.andExpect(jsonPath("$.entity.id", is(1)))
+				.andExpect(jsonPath("$.entity.customerName", is("Kalles Grustransporter")))
+				.andExpect(jsonPath("$.entity.customerAddress", containsString("Cement"))); // string(containsString((("Hello
+																							// World")))));
+	}
 
-	   /*@Test
-	   public void getArrivalsById() throws Exception {
-	       Arrival arrival = new Arrival();
-	       arrival.setCity("Yerevan");
+	@Test
+	public void shouldReturnNotFoundStatusAndNullObject() throws Exception {
+		this.mvc.perform(get("/customer").param("id", "7")).andDo(print()).andExpect(status().isOk())
+				.andExpect(jsonPath("$.status", is(404))).andExpect(jsonPath("$.entity").value(IsNull.nullValue()));
+	}
 
-	       given(arrivalController.getArrivalById(arrival.getId())).willReturn(arrival);
+	@Test
+	public void shouldReturnBadRequest() throws Exception {
+		this.mvc.perform(get("/customer").param("id", "r")).andDo(print()).andExpect(status().isBadRequest());
+	}
 
-	       mvc.perform(get(VERSION + ARRIVAL + arrival.getId())
-	               .with(user("blaze").password("Q1w2e3r4"))
-	               .contentType(APPLICATION_JSON))
-	               .andExpect(status().isOk())
-	               .andExpect(jsonPath("city", is(arrival.getCity())));
-	   }*/
-    
+	@Test
+	public void shouldUpdateCustomerDataSuccessfully() throws Exception {
+
+		createCustomer("99", "Ema", "Hype Park");
+
+		JSONObject jsonObject = new JSONObject();
+		jsonObject.accumulate("id", "99");
+		jsonObject.accumulate("customerName", "Salem");
+		jsonObject.accumulate("customerAddress", "Garden city");
+
+		this.mvc.perform(post("/customer").accept(MediaType.APPLICATION_JSON).content(jsonObject.toString())
+				.contentType(MediaType.APPLICATION_JSON)).andDo(print()).andExpect(status().isOk());
+
+		this.mvc.perform(get("/customer").param("id", "99")).andDo(print()).andExpect(status().isOk())
+				.andExpect(jsonPath("$.entity.id", is(99))).andExpect(jsonPath("$.entity.customerName", is("Salem")))
+				.andExpect(jsonPath("$.entity.customerAddress", is("Garden city")));
+
+	}
+
+	@Test
+	public void shouldUpdateCustomerNameSuccessfully() throws Exception {
+
+		createCustomer("88", "Jack", "seinfie lopk");
+
+		JSONObject jsonObject = new JSONObject();
+		jsonObject.accumulate("id", "88");
+		jsonObject.accumulate("customerName", "Mariano");
+		jsonObject.accumulate("customerAddress1", "address 123"); // address is
+																	// wrongly
+																	// typed
+
+		this.mvc.perform(post("/customer").accept(MediaType.APPLICATION_JSON).content(jsonObject.toString())
+				.contentType(MediaType.APPLICATION_JSON)).andDo(print()).andExpect(status().isOk());
+
+		this.mvc.perform(get("/customer").param("id", "88")).andDo(print()).andExpect(status().isOk())
+				.andExpect(jsonPath("$.entity.id", is(88))).andExpect(jsonPath("$.entity.customerName", is("Mariano")))
+				.andExpect(jsonPath("$.entity.customerAddress", containsString("seinfie lopk")));
+
+	}
+
+	@Test
+	public void shouldCreateCustomerSuccessfully() throws Exception {
+
+		createCustomer("66", "Klaus", "adress 123");
+
+	}
+
+	private void createCustomer(String id, String name, String address) throws Exception {
+		JSONObject jsonObject = new JSONObject();
+		jsonObject.accumulate("id", id);
+		jsonObject.accumulate("customerName", name);
+		jsonObject.accumulate("customerAddress", address);
+
+		this.mvc.perform(put("/customer").accept(MediaType.APPLICATION_JSON).content(jsonObject.toString())
+				.contentType(MediaType.APPLICATION_JSON)).andDo(print()).andExpect(status().isOk());
+
+		this.mvc.perform(get("/customer").param("id", id)).andDo(print()).andExpect(status().isOk())
+				.andExpect(jsonPath("$.entity.id", is(Integer.valueOf(id))))
+				.andExpect(jsonPath("$.entity.customerName", is(name)))
+				.andExpect(jsonPath("$.entity.customerAddress", containsString(address)));
+	}
 
 }
